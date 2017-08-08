7816ff93d0afd727b6fe486b8bc2a883ff83c12d
Дом <@DESKTOP-7GF1H2I>
2017-08-08 21:34:47 +0500

    сообщения об ошибках

--- задание4/Program.cs
+++ задание4/Program.cs
@@ -12,12 +12,43 @@ namespace задание4
         {
             double e, x;
             Console.WriteLine("Введите точность е");
-            e = Convert.ToDouble(Console.ReadLine());
+            Vvod("точность", out e);//вводим точность е
+            Proverka("точность",ref e);
             x = deleniya(0, 1, e);   //Поиск корня на интервале (0;1) c заданной точностью
             Console.WriteLine("корень уравнения x^4+2x^3-x-1 = "+x);  //вывод результата
             Console.ReadLine();   
         }
-        
+        static double Vvod(string s, out double n)//проверка ввода
+        {
+            bool ok;
+            string buf;
+            do
+            {
+                Console.Write(s + " = ");
+                buf = Console.ReadLine();
+                ok = double.TryParse(buf, out n);
+                if (!ok) Console.WriteLine("Введите " + s + " заново");
+            } while (!ok);
+            return n;
+        }
+        static void Proverka(string s, ref double a)
+        {
+            bool ok = false;
+            string buf;
+            do
+            {
+                if (a > 0) ok = true;
+                else
+                {
+                    if (!ok) Console.WriteLine("\aВведите " + s + " заново");
+                    Console.Write(s + " = ");
+                    buf = Console.ReadLine();
+                    ok = double.TryParse(buf, out a);
+                    ok = false;
+                }
+            } while (!ok);
+        }
+
         static double f(double x)
         {
             return x*x*x*x+2*x*x*x-x-1;
